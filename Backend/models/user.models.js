e358e21e8a48d221c2ddbac342fcf544532e683a
waliulhasaniram <waliulhasaniram000@gmail.com>
2026-03-20 02:03:38 +0600

    user model

--- Backend/models/user.models.js
+++ Backend/models/user.models.js
@@ -28,3 +28,21 @@ const userSchema = new mongoose.Schema({
         type: String
     }
 }, { timestamps: true });
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '10d' });
+    return token;
+};
+
+userSchema.methods.comparePassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.static.hashPassword = async function(password) {
+    const salt = await bcrypt.genSalt(parseInt(process.env.saltRounds));
+    return await bcrypt.hash(password, salt);
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
